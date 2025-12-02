d8a15eaf2c62ec68e5738e6b5d5a6e336938410f
sangita <sang.thapa123@gmail.com>
2025-12-02 23:21:03 +0100

    learn how to creating a django project during class

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    print("======"
+    "Someone visited home page=======")
+    return HttpResponse ("This is home page")
